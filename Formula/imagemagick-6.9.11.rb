a842ad878e896edcd20a87a4534f5afb77f8b97b
Alexa <alexa.sparks@wpengine.com>
2020-11-06 15:02:23 -0600

    feat: add imagemagick@6 formulae

---
+++ Formula/imagemagick-6.9.11.rb
@@ -0,0 +1,83 @@
+# We rely on this formula for imagemagick in PHP 7.4.1
+# It is a copy of the formula from homebrew-core https://github.com/Homebrew/homebrew-core/blob/master/Formula/imagemagick@6.rb
+
+class ImagemagickAT6 < Formula
+    desc "Tools and libraries to manipulate images in many formats"
+    homepage "https://www.imagemagick.org/"
+    # Please always keep the Homebrew mirror as the primary URL as the
+    # ImageMagick site removes tarballs regularly which means we get issues
+    # unnecessarily and older versions of the formula are broken.
+    url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.11-35.tar.xz"
+    mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-35.tar.xz"
+    sha256 "dcbaa0a3762ae685b1debe0ad12852e775204829a363fb992d52ff7511341713"
+    license "ImageMagick"
+    head "https://github.com/imagemagick/imagemagick6.git"
+
+    livecheck do
+      url "https://www.imagemagick.org/download/"
+      regex(/href=.*?ImageMagick[._-]v?(6(?:\.\d+)+(?:-\d+)?)\.t/i)
+    end
+
+    bottle do
+      sha256 "682a350777b8e61a1cfa405bb3dca669415c70486ee4a3461e451364a3447369" => :catalina
+      sha256 "5ad8687ebf550774dbf315d86f8578c5be285f9f3f16bc61fb9c0ad968006197" => :mojave
+      sha256 "3b29df30df7114cb189e7907100e5281e60c44006bd4590e5d35a4c9a3323641" => :high_sierra
+    end
+
+    keg_only :versioned_formula
+
+    depends_on "pkg-config" => :build
+
+    depends_on "freetype"
+    depends_on "ghostscript"
+    depends_on "jpeg"
+    depends_on "libpng"
+    depends_on "libtiff"
+    depends_on "libtool"
+    depends_on "little-cms2"
+    depends_on "openjpeg"
+    depends_on "webp"
+    depends_on "xz"
+
+    skip_clean :la
+
+    def install
+      # Avoid references to shim
+      inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+
+      args = %W[
+        --enable-osx-universal-binary=no
+        --prefix=#{prefix}
+        --disable-dependency-tracking
+        --disable-silent-rules
+        --disable-opencl
+        --disable-openmp
+        --enable-shared
+        --enable-static
+        --with-freetype=yes
+        --with-modules
+        --with-webp=yes
+        --with-openjp2
+        --with-gslib
+        --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+        --without-fftw
+        --without-pango
+        --without-x
+        --without-wmf
+      ]
+
+      # versioned stuff in main tree is pointless for us
+      inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
+      system "./configure", *args
+      system "make", "install"
+    end
+
+    test do
+      assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+      # Check support for recommended features and delegates.
+      features = shell_output("#{bin}/convert -version")
+      %w[Modules freetype jpeg png tiff].each do |feature|
+        assert_match feature, features
+      end
+    end
+  end
